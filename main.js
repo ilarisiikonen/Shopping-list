93846c9c7e3f5c043a41137e0cbdc89d2e7ff354
Ilari Siikonen <ilari.siikonen@gmail.com>
2022-12-07 15:43:48 +0200

    set local storage

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
-//to do list kaltainen ostoslista
+//alustetaan ostoslista
+let shoppingList = [];
 
-//local storage, delete
 
+/* Ohjelma katsoo löytyykö local storagesta ostoslistaa. Jos lista löytyy, ohjelma hakee sen. Ja jos ei, se asetetaan sinne */
 
-if (localStorage.getItem("list") === null) {
-    let list = [];
-    localStorage.setItem("list", JSON.stringify(list));
+if (localStorage.getItem("shoppingList") === null) {
+    
+    localStorage.setItem("shoppingList", JSON.stringify(shoppingList));
 
     console.log("localstoragessa ei ollut tietoa mutta nyt on.");
-
+    console.log(shoppingList)
 } else {
-    
+    let shoppingList = localStorage.getItem("shoppingList")
+    console.log(shoppingList)
     console.log("localstoragessa on jo tietoa")
 }
 
@@ -18,25 +20,33 @@ if (localStorage.getItem("list") === null) {
 
 
 
-
 // LISÄÄ
-function add (list, item) {
-    list.push(item)
+function add (shoppingList, item) {
+    item = document.getElementById("newItem").value;
+ 
     console.log(item)
-    console.log(list)
+    
+    shoppingList.push(item)
+    
+    console.log(shoppingList)
+    localStorage.setItem("shoppingList", JSON.stringify(shoppingList));
 }
 
 
 
 //POISTA YKSI
+/* documetn.getElementById("delete").onclick = funtion(){
 
+} */
 
 
 
 
 //TYHJÄÄ KAIKKI
+
+/* funktio tyhjentää local storagen */
 function reset() {
-    console.log("tyhjennä")
-    localStorage.removeItem("list")
-    
+    console.log("Lista tyhjennetty local storagesta")
+    localStorage.removeItem("shoppingList")
+    /* window.location.reload(true); */
 }
