7bc49fb34696f8ebe0bff5d33dbffedac1566956
Ilari Siikonen <ilari.siikonen@gmail.com>
2022-12-09 09:39:08 +0200

    display toimii mut errorii pukkaa

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //alustetaan ostoslista
-let shoppingList = [];
+shoppingList = [];
 
 
 /* Ohjelma katsoo löytyykö local storagesta ostoslistaa. Jos lista löytyy, ohjelma hakee sen. Ja jos ei, se asetetaan sinne */
@@ -11,33 +11,77 @@ if (localStorage.getItem("shoppingList") === null) {
     console.log("localstoragessa ei ollut tietoa mutta nyt on.");
     console.log(shoppingList)
 } else {
-    let shoppingList = localStorage.getItem("shoppingList")
+    shoppingList = JSON.parse(localStorage.getItem("shoppingList"))
     console.log(shoppingList)
     console.log("localstoragessa on jo tietoa")
+    displayShoppings()
 }
 
 
 
 
 
-// LISÄÄ
+// ADD
 function add (shoppingList, item) {
-    item = document.getElementById("newItem").value;
-    console.log(item);
-    shoppingList.push(item);
-    console.log(shoppingList);
-    localStorage.setItem("shoppingList", JSON.stringify(shoppingList));
-    document.querySelector('#newItem').value = "";  
-    displayShoppings(item)
+
+    if (item === null) {
+        Document.getElementById("error").innerHTML = "errror"
+    } else {
+        item = document.getElementById("newItem").value;
+        console.log(item + "  lisätty item");
+        shoppingList.push(item);
+        console.log(shoppingList + " lista");
+        localStorage.setItem("shoppingList", JSON.stringify(shoppingList));
+
+        //clear input
+        document.querySelector('#newItem').value = "";  
+
+        // display
+        displayShoppings() 
+    }
+    
 }
 
-//näytä lista
-function displayShoppings(item) {
-    const listItem = document.createElement("div")
-    listItem.classList.add(item)
-    const list = document.getElementById("shoppingList")
-    list.appendChild(listItem)
+
+
+// DISPLAY
+function displayShoppings() {
+    const lista = document.getElementById("shoppingList")
+    lista.innerHTML = ""
+    shoppingList = JSON.parse(localStorage.getItem("shoppingList")) 
+    
+    shoppingList.forEach(element => {
+        const item = document.createElement("div")
+        item.classList.add("listItem")
+        item.innerHTML = element;
+        
+        lista.append(item)
+
+    });
+     
 }
+
+
+
+
+/* 
+// DISPLAY
+function displayShoppings(shoppingList) {
+    
+    console.log(shoppingList)
+
+    shoppingList.forEach(element => {
+        const item = document.createElement("div")
+        item.innerHTML = element;
+        const lista = document.getElementById("shoppingList")
+        lista.append(item)
+
+    });
+     
+} */
+
+
+
 
 
 
@@ -55,5 +99,5 @@ function displayShoppings(item) {
 function reset() {
     console.log("Lista tyhjennetty local storagesta")
     localStorage.removeItem("shoppingList")
-    /* window.location.reload(true); */
+    displayShoppings()
 }
