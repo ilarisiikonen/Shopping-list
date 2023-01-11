731dc8c089340abe526e1f1c1321ccc6e98c0a0f
Ilari Siikonen <ilari.siikonen@gmail.com>
2023-01-11 18:04:21 +0200

    .

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 shoppingList = [];
 
 //edellinen ostoslista
-previousShoppingList = []
+previousShoppingList = [];
 
-/* Ohjelma katsoo löytyykö local storagesta ostoslistaa. Jos lista löytyy, ohjelma hakee sen. Ja jos ei, se asetetaan sinne */
+/* Ohjelma katsoo löytyykö local storagesta ostoslistaa. Jos lista löytyy, ohjelma hakee sen. Ja jos ei, se asetetaan tyhjänä sinne */
 
 if (localStorage.getItem("shoppingList") === null) {
     
