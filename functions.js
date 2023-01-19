e44ea3cd83b609fd7783c936632ff945fdeef0a3
Ilari Siikonen <ilari.siikonen@gmail.com>
2023-01-19 15:11:11 +0200

    added functions.js

---
+++ functions.js
@@ -0,0 +1,101 @@
+// ADD
+const add = (shoppingList, item) => {
+    item = document.getElementById("newItem").value;
+
+    if (item === "") {
+        document.getElementById("error").innerHTML = "Input field is empty"
+    } else {
+        document.getElementById("error").innerHTML = ""
+        shoppingList.push(item);
+
+        localStorage.setItem("shoppingList", JSON.stringify(shoppingList));
+
+        //clear input
+        document.querySelector('#newItem').value = "";  
+
+        // display
+        displayShoppings() 
+    }
+    
+}
+
+
+/* add items with enter key */
+const field = document.getElementById('newItem');
+
+
+field.addEventListener("keyup", function(event) {
+    if (event.code == "Enter") {
+        event.preventDefault();
+        document.getElementById('enter').click();
+    }
+});
+
+
+
+// DISPLAY
+const displayShoppings = () => {
+    const lista = document.getElementById("shoppingList")
+    lista.innerHTML = ""
+    shoppingList = JSON.parse(localStorage.getItem("shoppingList")) 
+    if (shoppingList === null) {
+        console.log("List is empty")
+    } else {
+        shoppingList.forEach(element => {
+        const item = document.createElement("div")
+        item.classList.add("listItem")
+        item.classList.add("notCollectedItem")
+        item.innerHTML = element;
+        
+        lista.append(item) //add item to the list
+
+            //mark as collected
+            item.addEventListener('click', function(){
+            if (item.classList.contains("collectedItem")) {
+
+                item.classList.remove("collectedItem")
+                item.classList.add("notCollectedItem")
+            } else {
+
+                item.classList.remove("notCollectedItem")
+                item.classList.add("collectedItem")
+            }
+        })
+
+        //delete item form list
+        //double click works on mobile but not in dev tools "mobile mode"
+        item.addEventListener('dblclick', function(){
+            lista.removeChild(item)
+            console.log(shoppingList)
+            shoppingList.splice(shoppingList.indexOf(element), 1)
+            console.log(shoppingList)
+            localStorage.setItem("shoppingList", JSON.stringify(shoppingList))
+        })
+
+        });
+    }
+    
+}
+
+
+//  RESET
+//function set shoppingList as and empty array
+const reset = () => {
+
+    localStorage.setItem("previousShoppingList", JSON.stringify(shoppingList));
+
+    shoppingList = [];
+    localStorage.setItem("shoppingList", JSON.stringify(shoppingList))
+
+    console.log(shoppingList)
+    displayShoppings()
+}
+
+//  UNDO
+//function returns previous list
+const undo = () => {
+    shoppingList = JSON.parse(localStorage.getItem("previousShoppingList"))
+    localStorage.setItem("shoppingList", JSON.stringify(shoppingList))
+    console.log(shoppingList)
+    displayShoppings()
+}
--- main.js
+++ main.js
@@ -6,6 +6,7 @@ previousShoppingList = [];
 
 /* Ohjelma katsoo löytyykö local storagesta ostoslistaa. Jos lista löytyy, ohjelma hakee sen. Ja jos ei, se asetetaan tyhjänä sinne */
 
+
 if (localStorage.getItem("shoppingList") === null) {
     localStorage.setItem("shoppingList", JSON.stringify(shoppingList));
     console.log("localstoragessa ei ollut tietoa mutta nyt on.");
@@ -20,109 +21,8 @@ if (localStorage.getItem("shoppingList") === null) {
 
 
 
-// ADD
-function add (shoppingList, item) {
-    item = document.getElementById("newItem").value;
-
-    if (item === "") {
-        document.getElementById("error").innerHTML = "Input field is empty"
-    } else {
-        document.getElementById("error").innerHTML = ""
-        shoppingList.push(item);
-
-        localStorage.setItem("shoppingList", JSON.stringify(shoppingList));
-
-        //clear input
-        document.querySelector('#newItem').value = "";  
-
-        // display
-        displayShoppings() 
-    }
-    
-}
-
-
-/* add items with enter key */
-const field = document.getElementById('newItem');
-
-
-field.addEventListener("keyup", function(event) {
-    if (event.code == "Enter") {
-        event.preventDefault();
-        document.getElementById('enter').click();
-    }
-});
-
-
-
-// DISPLAY
-function displayShoppings() {
-    const lista = document.getElementById("shoppingList")
-    lista.innerHTML = ""
-    shoppingList = JSON.parse(localStorage.getItem("shoppingList")) 
-    if (shoppingList === null) {
-        console.log("List is empty")
-    } else {
-        shoppingList.forEach(element => {
-        const item = document.createElement("div")
-        item.classList.add("listItem")
-        item.classList.add("notCollectedItem")
-        item.innerHTML = element;
-        
-        lista.append(item) //add item to the list
-
-            //mark as collected
-            item.addEventListener('click', function(){
-            if (item.classList.contains("collectedItem")) {
-
-                item.classList.remove("collectedItem")
-                item.classList.add("notCollectedItem")
-            } else {
-
-                item.classList.remove("notCollectedItem")
-                item.classList.add("collectedItem")
-            }
-        })
-
-        //delete item form list
-        //double click works on mobile but not in dev tools "mobile mode"
-        item.addEventListener('dblclick', function(){
-            lista.removeChild(item)
-            console.log(shoppingList)
-            shoppingList.splice(shoppingList.indexOf(element), 1)
-            console.log(shoppingList)
-            localStorage.setItem("shoppingList", JSON.stringify(shoppingList))
-        })
-
-        });
-    }
-    
-}
 
 
 
 
 
-
-
-//  RESET
-//function set shoppingList as and empty array
-function reset() {
-
-    localStorage.setItem("previousShoppingList", JSON.stringify(shoppingList));
-
-    shoppingList = [];
-    localStorage.setItem("shoppingList", JSON.stringify(shoppingList))
-
-    console.log(shoppingList)
-    displayShoppings()
-}
-
-//  UNDO
-//function returns previous list
-function undo() {
-    shoppingList = JSON.parse(localStorage.getItem("previousShoppingList"))
-    localStorage.setItem("shoppingList", JSON.stringify(shoppingList))
-    console.log(shoppingList)
-    displayShoppings()
-}
